b986c16dceecdc2936e3c7d5a2f337b924f29448
paulbobev <paulbobev@ufl.edu>
2025-04-10 15:18:30 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -10,13 +10,19 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    ####### Partner 1
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(2, 3), 6)          
+        self.assertEqual(mul(-1, 5), -5)   
+        self.assertEqual(mul(0, 100), 0)        
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(10, 2), 5)            
+        self.assertAlmostEqual(div(7, 3), 2.3333, 4)  
+        with self.assertRaises(ZeroDivisionError):      
+            div(5, 0)
+
+    ##########################
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,22 +39,23 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    ####### Partner 1
+    def test_log_invalid_argument(a,b): # 1 assertion
+        if a <= 0 or a == 1:
+            raise ValueError("Base 'a' must be positive and not equal to 1.")
+        if b <= 0:
+            raise ValueError("Argument 'b' must be positive.")
+        return math.log(b, a)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_hypotenuse(a,b): # 3 assertions
+        return math.sqrt(a ** 2 + b ** 2)
+
+
+    def test_sqrt(x): # 3 assertions
+        if self < 0:
+            raise ValueError("Cannot take square root of negative number.")
+        return math.sqrt(x)
     ##########################
 
 # Do not touch this
