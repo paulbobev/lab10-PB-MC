819eed7a7861cc2d0f0bb19abf15212b3a5932dc
paulbobev <paulbobev@ufl.edu>
2025-04-10 15:09:28 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,28 +5,27 @@ calculator.py
 One function per operation, in order.
 """
 import math
+
+
 # First example
 def add(a, b): 
-    return a + b
-def subtract(a, b): 
-    return a - b
-def subtract(a, b):
-    return a - b
-def multiply(a, b):
-    return a * b
-def divide(a, b):   # raise ZeroDivisionError if a == 0
-    return b / a
-def logarithm(a, b):     # use math library/raise ValueError
-    return math.log(a,b)
-def exponent(a, b): 
-    return math.pow(a,b)
-def square_root(a): 
-    try:
-        return math.sqrt(a) # raise ValueError if a < 0
-    except:
-        print("error")
-try:
-    def hypotenuse(a, b): 
-        return math.hypot(a, b) # can have negative nums
-except:
-    print("No Negative Numbers")
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return (b / a)
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError("Values must be positive.")
+    return math.log(a, b)
+
+def exp(a, b):
+    return (a ** b)
